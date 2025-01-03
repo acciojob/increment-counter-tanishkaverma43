49f99449f796671fe8d3d49918d801dcad9485cf
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-03 17:36:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-//your JS code here. If required.
-let button = document.getElementById('incrementBtn');
-let counter = document.getElementById('counter');
+window.onload = function() {
+    let button = document.getElementById('incrementBtn');
+    let counter = document.getElementById('counter');
 
-button.onclick = function() {
-    let currentValue = parseInt(counter.innerText);
-    counter.innerText = currentValue + 1;
+    button.onclick = function() {
+        let currentValue = parseInt(counter.innerText);
+        counter.innerText = currentValue + 1;
+    }
 }
