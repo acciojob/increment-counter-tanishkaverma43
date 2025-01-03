31814e4a4f851fdc87d1ea3327ac31338d12d418
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-03 17:39:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-window.onload = function() {
+document.addEventListener('DOMContentLoaded', (event) => {
     let button = document.getElementById('incrementBtn');
     let counter = document.getElementById('counter');
 
-    button.onclick = function() {
+    button.addEventListener('click', function() {
         let currentValue = parseInt(counter.innerText);
         counter.innerText = currentValue + 1;
-    }
-}
+    });
+});
